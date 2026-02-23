799a8b0e70711415d22c4522f0b93d603b5afe34
tomas-repos <tomas.reca@gmail.com>
2026-02-23 17:39:20 -0300

    change de hash

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';  
+import { provideRouter, withHashLocation } from '@angular/router';  
 import { AppRoot } from './app/app-root/app-root';
 import { routes } from './app/routes';
 
@@ -9,7 +9,7 @@ const apiUrl = 'https://rickandmortyapi.com/api';
 bootstrapApplication(AppRoot, {
   providers: [
     provideHttpClient(),
-    provideRouter(routes),
+    provideRouter(routes,withHashLocation()),
     { provide: 'API_URL', useValue: apiUrl }
   ]
 }).catch(err => console.error(err));
